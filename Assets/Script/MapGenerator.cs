b0769f632ce9c7ffc96fd4aeadb8606e9006d533
Nephrys <emylim175@gmail.com>
2025-12-17 10:27:37 +0100

    Generation de cube sur tile

--- Assets/Script/MapGenerator.cs
+++ Assets/Script/MapGenerator.cs
@@ -3,9 +3,46 @@ using System.Collections;
 
 public class MapGenerator : MonoBehaviour
 {
+
+    // a redefinir, calculer size plane/size tank
+    public int col;
+    public int row;
+    public int[,] Tile_binary;
+    public GameObject obstacle; 
+    public int cellSize;
+    public float origin;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Tile_binary = new int[col, row];
+        for (int x = 0; x < col; x++)
+        {
+            for (int y = 0; y < row; y++)
+            {
+                //Tile_binary[x,y] = Random.Range(0,2);
+                Tile_binary[x,y] = 0;
+            }
+        }
+        
+        // 2) Spawn a cube for each cell == 1
+        for (int x = 0; x < col; x++)
+        {
+            for (int y = 0; y < row; y++)
+            {
+                if (Tile_binary[x, y] == 0)
+                {
+                    // Compute world position on XZ-plane
+                    Vector3 pos = new Vector3(x * cellSize, 0f, y * cellSize);
+
+                    // Place and parent it
+                    GameObject newObs = Instantiate(obstacle,pos, Quaternion.identity );
+                    newObs.transform.SetParent(transform, worldPositionStays: true);
+                }
+            }
+        }
+        origin += 50;
+       
+
     }
 
     // Update is called once per frame
