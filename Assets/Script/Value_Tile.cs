ff38954835b59c4179d90f759c78ba84772abd1d
Rem9007 <remi.rodiere@gmail.com>
2025-12-17 12:13:49 +0100

    v1.1

---
+++ Assets/Script/Value_Tile.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Value_Tile : MonoBehaviour
+{
+    public float size;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
